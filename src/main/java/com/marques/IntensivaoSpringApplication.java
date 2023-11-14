0ecac355bbb9d56cd8c022fad9642935fcf54c9e
Marques <marques@marques.com>
2023-11-14 20:20:01 -0300

    Primeiro commit

---
+++ src/main/java/com/marques/IntensivaoSpringApplication.java
@@ -0,0 +1,13 @@
+package com.marques;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntensivaoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntensivaoSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marques/IntensivaoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marques;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntensivaoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
